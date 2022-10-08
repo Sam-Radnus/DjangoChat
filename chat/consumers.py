43504744be1d7c49dbeff7e74d722a4240e31a04
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-10-09 00:49:35 +0530

    refactor:updated CSS and Added Features

--- chat/consumers.py
+++ chat/consumers.py
@@ -19,19 +19,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        username = text_data_json['username']
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type':'chat_message',
+                'username':username,
                 'message':message
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-
+        username = event['username']
         await self.send(text_data=json.dumps({
             'type':'chat',
+            'username':username,
             'message':message
         }))
